0bc16272be797e4d16aa0ac58aac06262132a0a9
nurullokh <nnabijonov@students.wiut.uz>
2022-04-09 13:50:02 +0500

    Cors is fixed

--- src/config/settings/base.py
+++ src/config/settings/base.py
@@ -200,4 +200,10 @@ EMAIL_USE_TLS = True
 SENDER_EMAIL = "notifications@mt-servis.tech"
 
 BLOG_SHORT_LINK = os.environ.get("BLOG_SHORT_LINK")
-CORS_ALLOW_ALL_ORIGINS = True
+CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOW_CREDENTIALS = True
+CORS_ALLOWED_ORIGIN = ["http://localhost:3000", "http://localhost:3011"]
+CORS_ORIGIN_REGEX_WHITELIST = [
+    "http://localhost:3000",
+    "http://localhost:3011",
+]
