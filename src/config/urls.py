02e71f780e299ba64447caff08cfeb4c4315f757
nurullokh <nnabijonov@students.wiut.uz>
2022-04-11 11:53:40 +0500

    picture urls are fixed

--- src/config/urls.py
+++ src/config/urls.py
@@ -1,5 +1,3 @@
-from django.conf import settings
-from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
@@ -11,4 +9,4 @@ urlpatterns = [
     path("v1/", include("order.urls")),
     path("v1/", include("technician.urls")),
     path("v1/", include("blog.urls")),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
